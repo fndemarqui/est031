4e86eff12b5d563753ba13b22a11c08e3c29d086
fndemarqui <fndemarqui@gmail.com>
2021-01-07 08:43:35 -0300

    update cdf function

--- R/utils.R
+++ R/utils.R
@@ -21,35 +21,49 @@ cv <- function(x, perc = TRUE, ...){
 #' Cumulative distribution function
 #' @aliases cdf
 #' @export
-#' @description This function plots the cumulative distribution function (c.d.f) of a random variable.
-#' @param x a numeric vector with the observed values of the random variable.
-#' @param Fx if NULL (default), then Fx is computed based one of the implemented distributions (currently the binomial, poisson and geometric distributions for discrete random variables, or uniform, exponential and normal distribution for continuous random variables); otherwise the user must pass the values of Fx.
-#' @param ... further arguments passed to or from other methods.
-#' @return the desired plot of the cumulative distribution function.
+#' @description This function plots the cumulative distribution function (c.d.f) of a random variable
+#' @param x a numeric vector with the observed values of the random variable
+#' @param Fx if NULL (default), then Fx is computed using one of the implemented distributions (see description of the dist argument below); otherwise the user must pass the values of Fx
+#' @param type type of random variable; either discrete (default) or continuous; this argument is only necessary when Fx is passed by the user
+#' @param dist optional; name of the distribution (currently the binomial, poisson and geometric distributions for discrete random variables, or uniform, exponential and normal distribution for continuous random variables)
+#' @param ... further arguments passed to or from other methods
+#' @return the desired plot of the cumulative distribution function
 
-cdf <- function(x, Fx = NULL, dist = c("binom", "pois", "geom",
-                                       "unif", "exp", "norm"), ...){
+cdf <- function(x, Fx = NULL, type = c("discrete", "continuous"),
+                dist = c("binom", "pois", "geom","unif", "exp", "norm"), ...){
 
+  type <- match.arg(type)
   o <- order(x)
   x <- x[o]
 
   if(!is.null(Fx)){
+    Fx <- Fx[o]
     test <- length(x) == length(Fx)
     if(isTRUE(test)){
-      n2 <- length(x)
-      n1 <- n2-1
-      Fx <- Fx[o]
-      a <- abs(diff(x[1:2]))
-      b <- abs(diff(x[n1:n2]))
-      df <- data.frame(
-        x = c(x[1] - a, x, x[n2] + b),
-        Fx = c(0,Fx, 1)
-      )
+      if(type ==  "discrete"){
+        n2 <- length(x)
+        n1 <- n2-1
+        a <- abs(diff(x[1:2]))
+        b <- abs(diff(x[n1:n2]))
+        df <- data.frame(
+          x = c(x[1] - a, x, x[n2] + b),
+          Fx = c(0,Fx, 1)
+        )
+      }else{
+        type <- "continuous"
+        df <- data.frame(
+          x = x,
+          Fx = Fx
+        )
+      }
     }else{
       warning("x and Fx must be of the same length!")
     }
   }else{
     dist <- match.arg(dist)
+    if(dist == "unif" | dist == "exp" | dist == "norm"){
+      type <- "continuous"
+    }
     if(dist == "binom"){
       x = c(-1, x, max(x)+1)
       df <- data.frame(
@@ -84,10 +98,11 @@ cdf <- function(x, Fx = NULL, dist = c("binom", "pois", "geom",
         Fx = pnorm(x, ...)
       )
     }
-
   }
 
-  if(dist %in% c("binom", "pois", "geom")){
+
+
+  if(type == "discrete"){
     ggplot(df, aes(x=x, y = Fx)) +
       geom_point() +
       geom_step() +
