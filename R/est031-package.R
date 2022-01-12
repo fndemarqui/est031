b810135a443d373d700357686ab1282aad5e8853
fndemarqui <fndemarqui@gmail.com>
2022-01-12 06:00:24 -0300

    inclusion of shiny app tcl_prop

---
+++ R/est031-package.R
@@ -0,0 +1,10 @@
+#' The 'est031' package.
+#'
+#' @description Datasets and functions for the discipline Estatística e Probabilidades (EST031).
+#' @docType package
+#' @name est031-package
+#' @aliases est031-package
+#' @import dplyr ggplot2 shiny
+#' @importFrom stats dnorm pbinom pexp pgeom pnorm ppois punif rbinom sd
+#'
+NULL
